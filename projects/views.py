6c2ff6b9e97993218d28f7aeb4169599bc1d78f6
jacknely <jackn926@gmail.com>
2019-12-08 15:59:15 +0000

    initial commit initial commit

---
+++ projects/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from projects.models import Project
+
+
+def project_index(request):
+    projects = Project.objects.all()  # query db
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(id=pk)  # query db
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
