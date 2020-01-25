dc79a8c9b47bfa35672294775b0a068496786748
Jack Nelson <jackn926@gmail.com>
2020-01-25 04:46:52 +0000

    updated settings

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -138,6 +138,7 @@ STATICFILES_DIRS = (
 AWS_ACCESS_KEY_ID = os.environ.get('AKIASA2VMTNLGFDKBLGW')
 AWS_SECRET_ACCESS_KEY = os.environ.get('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')
 AWS_STORAGE_BUCKET_NAME = 'jacknelson-heroku'
+AWS_QUERYSTRING_AUTH = False
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
