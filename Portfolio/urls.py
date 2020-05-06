01dc9934febebeadc49e93829a7c1911057ece2e
Jack <jackn926@live.co.uk>
2020-05-06 22:56:15 +0100

    image issues

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path("projects/", include("projects.urls")),
     path('contact/', TemplateView.as_view(template_name="contact.html")),
     path('cv/', TemplateView.as_view(template_name="cv.html")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
